99b8801d92a986d885066afc1929643d646b1dc3
Charntent <752293795@qq.com>
2019-03-27 19:05:36 +0800

    First Test

---
+++ http/server.php
@@ -0,0 +1,32 @@
+<?php
+
+use Swoole\Http\Server;
+
+//ReflectionClass::export('Swoole\\Http\\Server');exit();
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+//
+//try {
+//    $http = new Server("127.0.0.1", 9501);
+//    var_dump($http);
+//    $http->on('request', function ($request, $response) {
+//        $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+//    });
+//    $http->start();
+//    //var_dump($http);
+//}catch (\Exception $e) {
+//    echo $e->getMessage();exit();
+//}
+
+$http = new swoole_http_server("127.0.0.1", 9501);
+
+$http->on("start", function ($server) {
+    echo "Swoole http server is started at http://127.0.0.1:9501\n";
+});
+
+$http->on("request", function ($request, $response) {
+    $response->header("Content-Type", "text/plain");
+    $response->end("Hello World\n");
+});
+
+$http->start();
